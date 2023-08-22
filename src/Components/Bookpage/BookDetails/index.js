379263372635a7fc6fa4fce5c3353d1ee1748ea5
Frankie Dick <fhd96@hotmail.co.uk>
2023-08-22 15:12:48 +0100

    made BookDetails component and passed props down to display all book details

---
+++ src/Components/Bookpage/BookDetails/index.js
@@ -0,0 +1,19 @@
+function BookDetails({image, title, author, year, pageCount, genre, blurb}) {
+    return ( 
+        <>
+            <div>
+                <img src={image} alt='image of book cover' />
+            </div>
+            <div>
+                <h2>{title}</h2>
+                <p>{author}</p>
+                <p>{year}</p>
+                <p>{pageCount}</p>
+                <p>{genre.name}</p>
+                <p>{blurb}</p>
+            </div>
+        </>
+    )
+}
+
+export default BookDetails
--- src/Components/Bookpage/index.js
+++ src/Components/Bookpage/index.js
@@ -1,20 +1,39 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import BookDetails from "./BookDetails"
 
 function Bookpage() {
 
     const {id} = useParams()
+    const [image, setImage] = useState('')
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [year, setYear] = useState('')
+    const [pageCount, setPageCount] = useState('')
+    const [genre, setGenre] = useState('')
+    const [blurb, setBlurb] = useState('')
+    // const [error, setError] = useState(false)
 
     useEffect(() => {
         fetch('https://book-swap-api.dev.io-academy.uk/api/books/' + id)
             .then(res => res.json())
             .then(bookData => {
                 console.log(bookData)
+                setImage(bookData.data.image)
+                setTitle(bookData.data.title)
+                setAuthor(bookData.data.author)
+                setYear(bookData.data.year)
+                setPageCount(bookData.data.page_count)
+                setGenre(bookData.data.genre)
+                setBlurb(bookData.data.blurb)
+                
             })
     },[])
 
     return (
-        <p>Bookpage</p>
+        <div>
+            <BookDetails image={image} title={title} author={author} year={year} pageCount={pageCount} genre={genre} blurb={blurb} />
+        </div>
     )
 }
 
