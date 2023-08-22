e425cc38f627b81411a4c216b021788bdb8d1f8a
Bert Rowett <bertrowett@googlemail.com>
2023-08-22 17:04:19 +0200

    Created ternary for displaying error vs book details- all working

--- src/Components/Bookpage/index.js
+++ src/Components/Bookpage/index.js
@@ -12,27 +12,33 @@ function Bookpage() {
     const [pageCount, setPageCount] = useState('')
     const [genre, setGenre] = useState('')
     const [blurb, setBlurb] = useState('')
-    // const [error, setError] = useState(false)
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         fetch('https://book-swap-api.dev.io-academy.uk/api/books/' + id)
             .then(res => res.json())
             .then(bookData => {
-                console.log(bookData)
-                setImage(bookData.data.image)
-                setTitle(bookData.data.title)
-                setAuthor(bookData.data.author)
-                setYear(bookData.data.year)
-                setPageCount(bookData.data.page_count)
-                setGenre(bookData.data.genre)
-                setBlurb(bookData.data.blurb)
                 
+                if (bookData.message !== 'Book successfully found') {
+                    setError(!error)
+                } else {
+                    console.log(bookData)
+                    setImage(bookData.data.image)
+                    setTitle(bookData.data.title)
+                    setAuthor(bookData.data.author)
+                    setYear(bookData.data.year)
+                    setPageCount(bookData.data.page_count)
+                    setGenre(bookData.data.genre)
+                    setBlurb(bookData.data.blurb)
+                }
+
             })
     },[])
 
     return (
         <div>
-            <BookDetails image={image} title={title} author={author} year={year} pageCount={pageCount} genre={genre} blurb={blurb} />
+            {error ? <p>Error, book not found</p> :
+            <BookDetails image={image} title={title} author={author} year={year} pageCount={pageCount} genre={genre} blurb={blurb} />}
         </div>
     )
 }
