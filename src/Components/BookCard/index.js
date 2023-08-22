96266af90593cd6d47589611c72bf7d0f4678e91
Vladi <vsstanchev@gmail.com>
2023-08-22 16:25:53 +0100

    Create Bookshelf component

--- src/Components/BookCard/index.js
+++ src/Components/BookCard/index.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from "react";
-import "./bookcard.css";
+import './bookcard.css';
 
 function BookCard({ bookCover, title, author, genre }) {
-  return (
-    <div>
-      <img src={bookCover} alt={title + " cover"} />
-      <h2>{title}</h2>
-      <p>{author}</p>
-      <p>{genre}</p>
-    </div>
-  );
+    return (
+        <div className='bookcard'>
+            <img src={bookCover} alt='' />
+            <h2>{title}</h2>
+            <p>{author}</p>
+            <p>{genre}</p>
+        </div>
+    );
 }
 
 export default BookCard;
