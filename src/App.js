fd60666cad453c3da4cec4837012a7abec9eee28
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-08-22 11:18:00 +0100

    Add CSS reset and  Navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} />
           <Route path="/book/:id" element={<Book />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
