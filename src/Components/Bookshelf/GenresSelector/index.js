67f82cfff511b0e7cc96b4a06b96287bfa1fc364
Vladi <vsstanchev@gmail.com>
2023-10-23 19:43:24 +0100

    Add filter by genre dropdown

---
+++ src/Components/Bookshelf/GenresSelector/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+function GenresSelector(props) {
+    const [genres, setGenres] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/api/genres')
+            .then((res) => res.json())
+            .then((genres) => {
+                setGenres(genres.data);
+            });
+    }, []);
+
+    function onGenreChange(genreId) {
+        console.log(genreId);
+        props.onGenreChangeID(genreId);
+    }
+
+    return (
+        <div>
+            <label htmlFor='genreId'>Filter by genre:</label>
+            <select
+                id='genreId'
+                onChange={(e) => onGenreChange(e.target.value)}
+            >
+                {genres.map((genre) => (
+                    <option key={genre.id} value={genre.id}>
+                        {genre.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
+
+export default GenresSelector;
--- src/Components/Bookshelf/index.js
+++ src/Components/Bookshelf/index.js
@@ -1,33 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import './bookshelf.css';
 import BookCard from '../BookCard';
+import GenresSelector from './GenresSelector';
 
 function Bookshelf({ claimed }) {
     const [bookCollection, setBookCollection] = useState([]);
+    const [selectedGenreId, setSelectedGenreId] = useState(''); // State to hold the selected genre
 
     useEffect(() => {
-        fetch('http://localhost:8000/api/books?claimed=' + claimed)
+        fetch(
+            `http://localhost:8000/api/books?claimed=${claimed}&genre=${selectedGenreId}`
+        )
             .then((res) => res.json())
             .then((books) => {
                 setBookCollection(books.data);
             });
-    }, [claimed]);
+    }, [claimed, selectedGenreId]);
+
+    // Function to handle changes in the selected genre
+    const handleGenreChange = (genre) => {
+        setSelectedGenreId(genre);
+    };
 
     return (
-        <div className='bookshelf'>
-            {bookCollection.length < 1 && <p>No Books Found</p>}
-            {bookCollection.map((book) => {
-                return (
-                    <BookCard
-                        id={book.id}
-                        bookCover={book.image}
-                        title={book.title}
-                        author={book.author}
-                        genre={book.genre.name}
-                        key={book.title}
-                    />
-                );
-            })}
+        <div>
+            <GenresSelector onGenreChangeID={handleGenreChange} />{' '}
+            {/* Pass the function to update selectedGenre state */}
+            <div className='bookshelf'>
+                {bookCollection == null && <p>No Books Found</p>}
+                {bookCollection?.map((book) => {
+                    return (
+                        <BookCard
+                            id={book.id}
+                            bookCover={book.image}
+                            title={book.title}
+                            author={book.author}
+                            genre={book.genre.name}
+                            key={book.title}
+                        />
+                    );
+                })}
+            </div>
         </div>
     );
 }
