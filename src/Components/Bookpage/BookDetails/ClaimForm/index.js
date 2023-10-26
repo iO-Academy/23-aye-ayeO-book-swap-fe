6fc89434ca14a62795c1cea9209ef2547a635aff
Vladi <vsstanchev@gmail.com>
2023-10-26 20:21:42 +0100

    Implement dialog element (modal) for claim/return book form

--- src/Components/Bookpage/BookDetails/ClaimForm/index.js
+++ src/Components/Bookpage/BookDetails/ClaimForm/index.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import "./claimform.css";
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './claimform.css';
 
-function ClaimForm({ getBookData }) {
+function ClaimForm({ getBookData, open, visibilityToggle }) {
     const { id } = useParams();
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
     const [nameError, setNameError] = useState(false);
     const [emailError, setEmailError] = useState(false);
 
@@ -43,64 +43,82 @@ function ClaimForm({ getBookData }) {
         }
     }
 
-    function handleSubmit() {
-        fetch("http://localhost:8000/api/books/claim/" + id, {
-            mode: "cors",
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body: JSON.stringify({
-                name: name,
-                email: email,
-            }),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.message !== `Book ${id} is already claimed`) {
-                    getBookData();
-                } else {
-                    alert("Book is already claimed!");
+    async function handleSubmit() {
+        try {
+            const res = await fetch(
+                'http://localhost:8000/api/books/claim/' + id,
+                {
+                    mode: 'cors',
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Accept: 'application/json',
+                    },
+                    body: JSON.stringify({
+                        name: name,
+                        email: email,
+                    }),
                 }
-            });
+            );
+
+            const data = await res.json();
+
+            if (data.message !== `Book ${id} is already claimed`) {
+                visibilityToggle();
+                getBookData();
+            } else {
+                throw new Error('Book is already claimed!');
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return (
-        <div className="form-container">
-            <form onSubmit={validateForm} className="claim-form">
-                <h3>Want to claim this book?</h3>
-                <div>
-                    <label htmlFor="name">Name</label>
+        <dialog open={open}>
+            <div className='form-container claim-return-form '>
+                <form onSubmit={validateForm} className='claim-form'>
+                    <h3>Want to claim this book?</h3>
+                    <div>
+                        <label htmlFor='name'>Name</label>
+
+                        <input
+                            type='text'
+                            id='name'
+                            name='name'
+                            placeholder='Name'
+                            value={name}
+                            onChange={changeName}
+                            className={nameError ? 'input-error' : ''}
+                        />
+                        <p className={nameError ? 'error' : 'hidden'}>
+                            Don't like your name
+                        </p>
+                    </div>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+
+                        <input
+                            type='email'
+                            name='email'
+                            placeholder='Email'
+                            value={email}
+                            onChange={changeEmail}
+                            className={emailError ? 'input-error' : ''}
+                        />
+                        <p className={emailError ? 'error' : 'hidden'}>
+                            Don't like your email
+                        </p>
+                    </div>
 
                     <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        placeholder="Name"
-                        value={name}
-                        onChange={changeName}
-                        className={nameError ? "input-error" : ""}
+                        type='submit'
+                        value='Claim'
+                        className='submit-button'
                     />
-                    <p className={nameError ? "error" : "hidden"}>Don't like your name</p>
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={changeEmail}
-                        className={emailError ? "input-error" : ""}
-                    />
-                    <p className={emailError ? "error" : "hidden"}>Don't like your email</p>
-                </div>
-
-                <input type="submit" value="Claim" className="submit-button" />
-            </form>
-        </div>
+                </form>
+            </div>
+        </dialog>
     );
 }
 
