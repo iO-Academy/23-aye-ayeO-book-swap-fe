b3a87a1780287603bdd977212ff733e2d2007e43
Vladi <vsstanchev@gmail.com>
2023-10-26 13:43:27 +0100

    Reverse reviews array so that the newest one is displayed first

--- src/Components/Bookpage/BookDetails/index.js
+++ src/Components/Bookpage/BookDetails/index.js
@@ -1,31 +1,51 @@
-import Review from "./Review";
-import ClaimForm from "./ClaimForm";
-import ReturnForm from "./ReturnForm";
-import ReviewForm from "./ReviewForm";
-import "./bookdetails.css";
+import Review from './Review';
+import ClaimForm from './ClaimForm';
+import ReturnForm from './ReturnForm';
+import ReviewForm from './ReviewForm';
+import './bookdetails.css';
 
-function BookDetails({ image, title, author, year, pageCount, genre, blurb, reviews, claimed, getBookData, refreshReviewsList }) {
-    const totalScore = reviews?.reduce((ratingSum, review) => ratingSum + review.rating, 0);
+function BookDetails({
+    image,
+    title,
+    author,
+    year,
+    pageCount,
+    genre,
+    blurb,
+    reviews,
+    claimed,
+    getBookData,
+    refreshReviewsList,
+}) {
+    const totalScore = reviews?.reduce(
+        (ratingSum, review) => ratingSum + review.rating,
+        0
+    );
     const avgScore = totalScore ? totalScore / reviews?.length : 0;
 
     return (
-        <div className="book-details">
-            <div className="book-image">
-                <img src={image} alt={"Cover of " + title} />
+        <div className='book-details'>
+            <div className='book-image'>
+                <img src={image} alt={'Cover of ' + title} />
             </div>
-            <div className="">
+            <div className=''>
                 <h2>{title}</h2>
                 <p>{author}</p>
                 <p>{year}</p>
                 {pageCount && <p>{pageCount} pages</p>}
                 <p>Genre: {genre.name}</p>
                 <p>
-                    <a href="#reviews">{reviews?.length} reviews</a> - {avgScore.toFixed(1)} / 5 stars
+                    <a href='#reviews'>{reviews?.length} reviews</a> -{' '}
+                    {avgScore.toFixed(1)} / 5 stars
                 </p>
                 {claimed && <p>Claimed by {claimed}</p>}
-                {claimed ? <ReturnForm getBookData={getBookData} claimed={claimed} /> : <ClaimForm getBookData={getBookData} />}
+                {claimed ? (
+                    <ReturnForm getBookData={getBookData} claimed={claimed} />
+                ) : (
+                    <ClaimForm getBookData={getBookData} />
+                )}
                 <p>{blurb}</p>
-                <h3 id="reviews">Reviews</h3>
+                <h3 id='reviews'>Reviews</h3>
                 <ReviewForm refreshReviewsList={refreshReviewsList} />
                 {reviews?.map((review) => (
                     <Review review={review} key={review.id} />
