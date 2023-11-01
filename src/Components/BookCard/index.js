599f86a874abca4572b7c8d54db99d9b04d72383
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-11-01 12:36:40 +0000

    Refactor ImgLoader to be generic

--- src/Components/BookCard/index.js
+++ src/Components/BookCard/index.js
@@ -1,34 +1,16 @@
-import { useState } from 'react';
-import './bookcard.css';
-import { Link } from 'react-router-dom';
-import ImgLoader from '../ImgLoader';
+import { useState } from "react";
+import "./bookcard.css";
+import { Link } from "react-router-dom";
+import ImgLoader from "../ImgLoader";
 
 function BookCard({ id, bookCover, title, author, genre }) {
-    const [isImageLoaded, setIsImageLoaded] = useState(false);
-
-    const handleImageLoad = () => {
-        setIsImageLoaded(true);
-    };
-
     return (
-        <Link
-            to={'/books/' + id}
-            className='bookcard w-72 p-6 bg-slate-100 rounded flex text-slate-800'
-        >
-            <div className='bookcard'>
-                {!isImageLoaded && <ImgLoader w='60' h='96' />}
-
-                <img
-                    src={bookCover}
-                    alt={title + ' cover'}
-                    className='rounded object-cover w-64 h-96 hover:w-96'
-                    onLoad={handleImageLoad}
-                    style={{ display: isImageLoaded ? 'block' : 'none' }}
-                />
-
-                <h2 className='pt-5 pb-1 text-lg font-semibold'>{title}</h2>
-                <p className='text-slate-600 text-sm'>by {author}</p>
-                <p className='text-slate-600 text-xs font-light'>{genre}</p>
+        <Link to={"/books/" + id} className="bookcard w-72 p-6 bg-slate-100 rounded flex text-slate-800">
+            <div className="bookcard">
+                <ImgLoader src={bookCover} alt={title + " cover"} w="60" h="96" />
+                <h2 className="pt-5 pb-1 text-lg font-semibold">{title}</h2>
+                <p className="text-slate-600 text-sm">by {author}</p>
+                <p className="text-slate-600 text-xs font-light">{genre}</p>
             </div>
         </Link>
     );
--- src/Components/Bookpage/BookDetails/index.js
+++ src/Components/Bookpage/BookDetails/index.js
@@ -1,27 +1,13 @@
-import Review from './Review';
-import ClaimForm from './ClaimForm';
-import ReturnForm from './ReturnForm';
-import ReviewForm from './ReviewForm';
-import './bookdetails.css';
-import { useState } from 'react';
+import Review from "./Review";
+import ClaimForm from "./ClaimForm";
+import ReturnForm from "./ReturnForm";
+import ReviewForm from "./ReviewForm";
+import "./bookdetails.css";
+import { useState } from "react";
+import ImgLoader from "../../ImgLoader";
 
-function BookDetails({
-    image,
-    title,
-    author,
-    year,
-    pageCount,
-    genre,
-    blurb,
-    reviews,
-    claimed,
-    getBookData,
-    refreshReviewsList,
-}) {
-    const totalScore = reviews?.reduce(
-        (ratingSum, review) => ratingSum + review.rating,
-        0
-    );
+function BookDetails({ image, title, author, year, pageCount, genre, blurb, reviews, claimed, getBookData, refreshReviewsList }) {
+    const totalScore = reviews?.reduce((ratingSum, review) => ratingSum + review.rating, 0);
 
     const avgScore = totalScore ? totalScore / reviews?.length : 0;
 
@@ -37,56 +23,47 @@ function BookDetails({
     }
 
     return (
-        <div className='book-details'>
-            <div className='book-image'>
-                <img src={image} alt={'Cover of ' + title} />
+        <>
+            <div className="book-details w-1/2 m-auto flex flex-col md:flex-row">
+                <ImgLoader src={image} alt={"Cover of " + title} w="60" h="96" />
+
+                <div className="">
+                    <h2 className="text-3xl font-bold underline">{title}</h2>
+                    <p>{author}</p>
+                    <p>{year}</p>
+                    {pageCount && <p>{pageCount} pages</p>}
+                    <p>Genre: {genre.name}</p>
+                    <p>
+                        <a href="#reviews">{reviews?.length} reviews</a> - {avgScore.toFixed(1)} / 5 stars
+                    </p>
+                    {claimed && <p>Claimed by {claimed}</p>}
+                    {claimed ? (
+                        <button data-element="return" onClick={toggleReturn}>
+                            Return
+                        </button>
+                    ) : (
+                        <button data-element="claim" onClick={toggleClaim}>
+                            Claim
+                        </button>
+                    )}
+
+                    {openReturn && claimed && (
+                        <ReturnForm getBookData={getBookData} claimed={claimed} open={openReturn} visibilityToggle={toggleReturn} />
+                    )}
+
+                    {openClaim && !claimed && <ClaimForm getBookData={getBookData} open={openClaim} visibilityToggle={toggleClaim} />}
+
+                    <p>{blurb}</p>
+                </div>
             </div>
-            <div className=''>
-                <h2 className='text-3xl font-bold underline'>{title}</h2>
-                <p>{author}</p>
-                <p>{year}</p>
-                {pageCount && <p>{pageCount} pages</p>}
-                <p>Genre: {genre.name}</p>
-                <p>
-                    <a href='#reviews'>{reviews?.length} reviews</a> -{' '}
-                    {avgScore.toFixed(1)} / 5 stars
-                </p>
-                {claimed && <p>Claimed by {claimed}</p>}
-                {claimed ? (
-                    <button data-element='return' onClick={toggleReturn}>
-                        Return
-                    </button>
-                ) : (
-                    <button data-element='claim' onClick={toggleClaim}>
-                        Claim
-                    </button>
-                )}
-
-                {openReturn && claimed && (
-                    <ReturnForm
-                        getBookData={getBookData}
-                        claimed={claimed}
-                        open={openReturn}
-                        visibilityToggle={toggleReturn}
-                    />
-                )}
-
-                {openClaim && !claimed && (
-                    <ClaimForm
-                        getBookData={getBookData}
-                        open={openClaim}
-                        visibilityToggle={toggleClaim}
-                    />
-                )}
-
-                <p>{blurb}</p>
-                <h3 id='reviews'>Reviews</h3>
+            <div>
+                <h3 id="reviews">Reviews</h3>
                 <ReviewForm refreshReviewsList={refreshReviewsList} />
                 {reviews?.map((review) => (
                     <Review review={review} key={review.id} />
                 ))}
             </div>
-        </div>
+        </>
     );
 }
 
