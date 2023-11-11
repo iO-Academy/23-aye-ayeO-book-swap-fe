db7fe77e436547897f9121721fe426af93a893b7
Vladi <vsstanchev@gmail.com>
2023-11-11 20:34:28 +0000

    Add ISBN validation utility

--- src/utilities.js
+++ src/utilities.js
@@ -28,3 +28,38 @@ export function isValidYear(year) {
     const yearPattern = /^(?!0{1,2})\d{3,4}$/;
     return yearPattern.test(year);
 }
+
+export function isValidISBN(isbn) {
+    isbn = isbn.replace(/[- ]/g, ''); // Remove hyphens and spaces
+
+    const length = isbn.length;
+
+    if (length === 10) {
+        // Validate ISBN-10
+        let sum = 0;
+        for (let i = 0; i < 10; i++) {
+            if (isbn[i] === 'X' && i === 9) {
+                sum += 10; // 'X' represents 10
+            } else if (!isNaN(parseInt(isbn[i]))) {
+                sum += parseInt(isbn[i]) * (10 - i);
+            } else {
+                return false; // Invalid character found
+            }
+        }
+        return sum % 11 === 0;
+    } else if (
+        length === 13 &&
+        (isbn.substring(0, 3) === '978' || isbn.substring(0, 3) === '979')
+    ) {
+        // Validate ISBN-13
+        let sum = 0;
+        for (let i = 0; i < 12; i++) {
+            const factor = i % 2 === 0 ? 1 : 3;
+            sum += parseInt(isbn[i]) * factor;
+        }
+        const checkDigit = (10 - (sum % 10)) % 10;
+        return parseInt(isbn[12]) === checkDigit;
+    } else {
+        return false; // Invalid length or format
+    }
+}
