a17e41e1384f73a5c683df9f9f27df75507c6328
Vladi <vsstanchev@gmail.com>
2023-11-12 01:06:48 +0000

    Refactor Title Capitalisation utility func

--- src/utilities.js
+++ src/utilities.js
@@ -63,6 +63,7 @@ export function isValidISBN(isbn) {
         return false; // Invalid length or format
     }
 }
+
 export function capitaliseTitle(title) {
     const articles = ['a', 'an', 'the'];
     const prepositions = ['at', 'by', 'for', 'in', 'of', 'on', 'to', 'with'];
@@ -72,16 +73,13 @@ export function capitaliseTitle(title) {
 
     for (let i = 0; i < words.length; i++) {
         if (
-            (i !== 0 &&
-                (articles.includes(words[i]) ||
-                    prepositions.includes(words[i]) ||
-                    conjunctions.includes(words[i]))) ||
-            (i === 0 &&
-                (articles.includes(words[i]) ||
-                    prepositions.includes(words[i])))
+            i === 0 ||
+            !(
+                articles.includes(words[i]) ||
+                prepositions.includes(words[i]) ||
+                conjunctions.includes(words[i])
+            )
         ) {
-            words[i] = words[i].toLowerCase();
-        } else {
             words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
         }
     }
