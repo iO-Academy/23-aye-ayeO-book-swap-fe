d7473b5b742e47d1f16e25fa90144885023b771a
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-11-14 18:06:07 +0000

    Add StarRating component

--- src/Components/Bookpage/BookDetails/Review/index.js
+++ src/Components/Bookpage/BookDetails/Review/index.js
@@ -1,9 +1,12 @@
+import StarRating from "../StarRating";
+
 function Review({ review }) {
     return (
         <div className="bg-white  border-slate-200 rounded-md my-4 flex">
-            <div className="flex-shrink-0 p-5 w-40 bg-rose-50 rounded-l-md">
+            <div className="flex-shrink-0 p-5 w-40 bg-zinc-50 rounded-l-md">
                 <h4 className="font-bold py-0">{review.name}</h4>
-                <p>{review.rating}/5 stars</p>
+
+                <StarRating rating={review.rating} />
             </div>
             <div className="flex-grow p-5 border-slate-200">
                 <p>{review.review}</p>
--- src/Components/Bookpage/BookDetails/StarRating/index.js
+++ src/Components/Bookpage/BookDetails/StarRating/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const StarRating = ({ rating }) => {
-    return <div className="Stars" style={{ "--rating": `${rating}` }} aria-label={`Rating of this product is ${rating} out of 5.`}></div>;
+    return (
+        <div
+            className="Stars"
+            style={{ "--rating": `${rating}` }}
+            aria-label={`Rating of this product is ${rating} out of 5.`}
+            role="presentation"
+        ></div>
+    );
 };
 
 export default StarRating;
