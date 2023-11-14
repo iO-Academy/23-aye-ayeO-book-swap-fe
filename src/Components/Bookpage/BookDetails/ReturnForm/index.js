aebefe0ef0045202eacfdf8c58e15db31f3aeed6
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-11-14 12:14:59 +0000

    add close button for claim/return form

--- src/Components/Bookpage/BookDetails/ReturnForm/index.js
+++ src/Components/Bookpage/BookDetails/ReturnForm/index.js
@@ -1,19 +1,13 @@
-import React, { useContext, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Context } from '../../../../Context';
-import { displayErrorMessage, isValidEmail } from '../../../../utilities';
+import React, { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../../../../Context";
+import { displayErrorMessage, isValidEmail } from "../../../../utilities";
 
-function ReturnForm({
-    claimed,
-    getBookData,
-    open,
-    visibilityToggle,
-    bookTitle,
-}) {
+function ReturnForm({ claimed, getBookData, open, visibilityToggle, bookTitle }) {
     const { id } = useParams();
-    const [email, setEmail] = useState('');
+    const [email, setEmail] = useState("");
     const [emailError, setEmailError] = useState(false);
-    const [serverError, setServerError] = useState('');
+    const [serverError, setServerError] = useState("");
 
     const { setAlert } = useContext(Context);
 
@@ -35,33 +29,30 @@ function ReturnForm({
 
         if (!emailError) {
             handleSubmit(e);
-            document.body.style.overflow = !open ? 'hidden' : 'auto';
+            document.body.style.overflow = !open ? "hidden" : "auto";
         }
     }
 
     async function handleSubmit() {
         try {
-            const res = await fetch(
-                'http://localhost:8000/api/books/return/' + id,
-                {
-                    mode: 'cors',
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Accept: 'application/json',
-                    },
-                    body: JSON.stringify({
-                        email: email,
-                    }),
-                }
-            );
+            const res = await fetch("http://localhost:8000/api/books/return/" + id, {
+                mode: "cors",
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                }),
+            });
 
             const data = await res.json();
 
             if (res.ok) {
                 visibilityToggle();
                 getBookData();
-                setAlert('Book returned');
+                setAlert("Book returned");
             } else {
                 throw new Error(data.message);
             }
@@ -76,46 +67,38 @@ function ReturnForm({
         }
     }
 
-    document.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape') {
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
             visibilityToggle();
         }
     });
 
     return (
-        <dialog
-            open={open}
-            className='dialog w-full fixed inset-0 flex items-center justify-center h-full'
-            onClick={backdropClick}
-        >
-            <div onSubmit={validateForm} className='form-container w-[500px]'>
-                <form className='claim-form'>
+        <dialog open={open} className="dialog w-full fixed inset-0 flex items-center justify-center h-full" onClick={backdropClick}>
+            <div onSubmit={validateForm} className="form-container w-[500px] relative">
+                <button onClick={backdropClick} className="absolute top-5 right-5 text-3xl text-zinc-600 material-symbols-outlined">
+                    close
+                </button>
+                <form className="claim-form">
                     <h3>Return "{bookTitle}"?</h3>
                     <br />
                     <div>
-                        <label htmlFor='email'> {claimed}'s Email</label>
+                        <label htmlFor="email"> {claimed}'s Email</label>
 
                         <input
-                            type='email'
-                            name='email'
+                            autoComplete="email"
+                            type="email"
+                            id="email"
+                            name="email"
                             value={email}
                             onChange={changeEmail}
-                            className={
-                                emailError
-                                    ? 'form-text input-error'
-                                    : 'form-text'
-                            }
+                            className={emailError ? "form-text input-error" : "form-text"}
                         />
-                        {emailError &&
-                            displayErrorMessage('Valid email is required')}
+                        {emailError && displayErrorMessage("Valid email is required")}
                         {serverError && displayErrorMessage(serverError)}
                     </div>
                     <br />
-                    <input
-                        type='submit'
-                        value='Return Book'
-                        className='button py-3'
-                    />
+                    <input type="submit" value="Return Book" className="button py-3" />
                 </form>
             </div>
         </dialog>
--- src/Components/Bookpage/BookDetails/index.js
+++ src/Components/Bookpage/BookDetails/index.js
@@ -1,28 +1,13 @@
-import Review from './Review';
-import ClaimForm from './ClaimForm';
-import ReturnForm from './ReturnForm';
-import ReviewForm from './ReviewForm';
-import './bookdetails.css';
-import { useState } from 'react';
-import ImgLoader from '../../ImgLoader';
+import Review from "./Review";
+import ClaimForm from "./ClaimForm";
+import ReturnForm from "./ReturnForm";
+import ReviewForm from "./ReviewForm";
+import "./bookdetails.css";
+import { useState } from "react";
+import ImgLoader from "../../ImgLoader";
 
-function BookDetails({
-    image,
-    title,
-    author,
-    year,
-    pageCount,
-    genre,
-    blurb,
-    reviews,
-    claimed,
-    getBookData,
-    refreshReviewsList,
-}) {
-    const totalScore = reviews?.reduce(
-        (ratingSum, review) => ratingSum + review.rating,
-        0
-    );
+function BookDetails({ image, title, author, year, pageCount, genre, blurb, reviews, claimed, getBookData, refreshReviewsList }) {
+    const totalScore = reviews?.reduce((ratingSum, review) => ratingSum + review.rating, 0);
 
     const avgScore = totalScore ? totalScore / reviews?.length : 0;
 
@@ -31,33 +16,28 @@ function BookDetails({
 
     function toggleClaim() {
         openClaim ? setOpenClaim(false) : setOpenClaim(true);
-        document.body.style.overflow = !openClaim ? 'hidden' : 'auto';
+        document.body.style.overflow = !openClaim ? "hidden" : "auto";
     }
 
     function toggleReturn() {
         openReturn ? setOpenReturn(false) : setOpenReturn(true);
-        document.body.style.overflow = !openReturn ? 'hidden' : 'auto';
+        document.body.style.overflow = !openReturn ? "hidden" : "auto";
     }
 
     return (
-        <div className='w-full max-w-7xl m-auto bg-zinc-100'>
-            <div className='book-details w-full m-auto p-20 flex flex-col md:flex-row justify-center gap-4'>
-                <div className='w-[600px]'>
-                    <ImgLoader
-                        src={image}
-                        alt={'Cover of ' + title}
-                        w='60'
-                        h='96'
-                    />
+        <div className="w-full max-w-7xl m-auto bg-zinc-100">
+            <div className="book-details w-full m-auto p-20 flex flex-col md:flex-row justify-center gap-4">
+                <div className="w-[600px]">
+                    <ImgLoader src={image} alt={"Cover of " + title} w="60" h="96" />
                 </div>
-                <div className=' p-2 flex flex-col gap-2 w-[1200px]'>
-                    <h1 className='text-left p-0 text-5xl'>{title}</h1>
+                <div className=" p-2 flex flex-col gap-2 w-[1200px]">
+                    <h1 className="text-left p-0 text-5xl">{title}</h1>
                     <p>{author}</p>
                     <br />
-                    <p className='text-sm'>
-                        {' '}
-                        {avgScore.toFixed(1)} / 5 stars |{' '}
-                        <a href='#reviews' className='underline'>
+                    <p className="text-sm">
+                        {" "}
+                        {avgScore.toFixed(1)} / 5 stars |{" "}
+                        <a href="#reviews" className="underline">
                             {reviews?.length} reviews
                         </a>
                     </p>
@@ -68,19 +48,11 @@ function BookDetails({
                     {pageCount && <p>{pageCount} pages</p>}
                     {claimed && <p>Claimed by {claimed}</p>}
                     {claimed ? (
-                        <button
-                            data-element='return'
-                            onClick={toggleReturn}
-                            className='button py-2 w-max'
-                        >
+                        <button data-element="return" onClick={toggleReturn} className="button py-2 w-max">
                             Return Book
                         </button>
                     ) : (
-                        <button
-                            data-element='claim'
-                            onClick={toggleClaim}
-                            className='button py-2 w-max'
-                        >
+                        <button data-element="claim" onClick={toggleClaim} className="button py-2 w-max">
                             Claim Book
                         </button>
                     )}
@@ -96,17 +68,12 @@ function BookDetails({
                     )}
 
                     {openClaim && !claimed && (
-                        <ClaimForm
-                            getBookData={getBookData}
-                            open={openClaim}
-                            visibilityToggle={toggleClaim}
-                            bookTitle={title}
-                        />
+                        <ClaimForm getBookData={getBookData} open={openClaim} visibilityToggle={toggleClaim} bookTitle={title} />
                     )}
 
                     <div>
                         <hr />
-                        <h2 className='text-center' id='reviews'>
+                        <h2 className="text-center" id="reviews">
                             Reviews
                         </h2>
                         {reviews?.map((review) => (
