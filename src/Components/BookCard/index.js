4b78264b8ac5e6f6ead34fed6b8db7fb225ad630
Vladi <vsstanchev@gmail.com>
2023-11-15 10:23:02 +0000

    Fix Image loader dimensions issue

--- src/Components/BookCard/index.js
+++ src/Components/BookCard/index.js
@@ -1,15 +1,25 @@
-import "./bookcard.css";
-import { Link } from "react-router-dom";
-import ImgLoader from "../ImgLoader";
+import './bookcard.css';
+import { Link } from 'react-router-dom';
+import ImgLoader from '../ImgLoader';
 
 function BookCard({ id, bookCover, title, author, genre }) {
     return (
-        <Link to={"/books/" + id} className="bookcard w-72 p-6 bg-zinc-100 rounded flex text-slate-800">
-            <div>
-                <ImgLoader src={bookCover} alt={title + " cover"} w="60" h="96" />
-                <h2 className="text-xl">{title}</h2>
-                <p className="text-slate-600 text-sm">by {author}</p>
-                <p className="text-slate-600 text-xs font-light">{genre}</p>
+        <Link
+            to={'/books/' + id}
+            className='sm:w-72 p-6 bg-zinc-100 rounded  text-slate-800 w-full'
+        >
+            <div className='flex sm:flex-col'>
+                <ImgLoader
+                    src={bookCover}
+                    alt={title + ' cover'}
+                    w='60'
+                    h='96'
+                />
+                <div>
+                    <h2 className='text-xl'>{title}</h2>
+                    <p className='text-slate-600 text-sm'>by {author}</p>
+                    <p className='text-slate-600 text-xs font-light'>{genre}</p>
+                </div>
             </div>
         </Link>
     );
