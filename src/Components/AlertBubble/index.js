d2f4e051a373f013c9ad821649f6dfbed8dfa381
Vladi <vsstanchev@gmail.com>
2023-11-15 23:46:01 +0000

    Improve alert features

--- src/Components/AlertBubble/index.js
+++ src/Components/AlertBubble/index.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 function AlertBubble({ message }) {
-    const [isVisible, setIsVisible] = useState("-translate-y-[800px]");
+    const [isVisible, setIsVisible] = useState('-translate-y-full');
 
     useEffect(() => {
         if (message) {
-            setIsVisible("translate-y-0");
+            setIsVisible('translate-y-0');
 
             const timer = setTimeout(() => {
-                setIsVisible("-translate-y-[800px]");
-            }, 3000);
+                setIsVisible('-translate-y-full');
+            }, 2000);
 
             return () => {
                 clearTimeout(timer);
@@ -19,10 +19,7 @@ function AlertBubble({ message }) {
 
     return (
         <div
-            className={`rounded-xl bg-emerald-300 alert-bubble transition fixed z-50 mx-auto  w-full text-center py-10 ${
-                // isVisible ? "visible" : "hidden"
-                isVisible
-            }`}
+            className={`rounded-b-xl bg-[#202124] alert-bubble transition-all ease-in-out duration-600 fixed z-50 mx-auto  w-full text-center text-zinc-200 text-2xl py-10 ${isVisible}`}
         >
             {message}
         </div>
