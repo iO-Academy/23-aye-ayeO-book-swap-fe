88846bf58a04f1c71e3e52679337de8d00d9bf26
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-11-16 14:46:43 +0000

    Fix AlertBubble position

--- src/Components/AlertBubble/index.js
+++ src/Components/AlertBubble/index.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function AlertBubble({ message }) {
-    const [position, setPosition] = useState('-translate-y-full');
+    const [position, setPosition] = useState("-translate-y-[218px]");
     useEffect(() => {
         if (message) {
-            setPosition('translate-y-0');
+            setPosition("-translate-y-[122px] sm:translate-y-0");
 
             const timer = setTimeout(() => {
-                setPosition('-translate-y-full');
+                setPosition("-translate-y-[218px]");
             }, 2000);
 
             return () => {
@@ -18,7 +18,7 @@ function AlertBubble({ message }) {
 
     return (
         <div
-            className={`rounded-b-xl bg-[#202124] alert-bubble transition-all ease-in-out duration-600 fixed z-50 mx-auto  w-full text-center text-zinc-200 text-2xl py-10 ${position}`}
+            className={`rounded-b-xl bg-[#71FBE7] alert-bubble transition-all ease-in-out duration-600 fixed z-50 mx-auto  w-full text-center text-zinc-700 text-2xl py-12 ${position}`}
         >
             {message}
         </div>
