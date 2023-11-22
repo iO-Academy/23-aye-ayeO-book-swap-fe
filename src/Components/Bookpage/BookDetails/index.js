839bc5764ffef0ac8c11481af21c8e0ad6e19259
Vladi <vsstanchev@gmail.com>
2023-11-22 12:47:00 +0000

    Add ScrollToTop component

--- src/Components/Bookpage/BookDetails/index.js
+++ src/Components/Bookpage/BookDetails/index.js
@@ -6,6 +6,7 @@ import './bookdetails.css';
 import { useState } from 'react';
 import ImgLoader from '../../ImgLoader';
 import StarRating from './StarRating';
+import ScrollToTop from '../../ScrollToTop';
 
 function BookDetails({
     image,
@@ -38,14 +39,16 @@ function BookDetails({
     }
 
     return (
-        <div className='w-full min-h-screen overflow-hidden max-w-7xl m-auto bg-zinc-100'>
-            <div className='book-details w-full m-auto p-5 sm:p-20 pt-16 sm:pt-32 flex flex-col lg:flex-row justify-center lg:gap-24 gap-10'>
-                <div className='w-[400px] flex justify-center max-lg:self-center'>
-                    <div className='lg:fixed lg:z-40'>
-                        <ImgLoader
-                            src={image}
-                            alt={'Cover of ' + title}
-                            dimensions='
+        <>
+            <ScrollToTop />
+            <div className='w-full min-h-screen overflow-hidden max-w-7xl m-auto bg-zinc-100'>
+                <div className='book-details w-full m-auto p-5 sm:p-20 pt-16 sm:pt-32 flex flex-col lg:flex-row justify-center lg:gap-24 gap-10'>
+                    <div className='w-[400px] flex justify-center max-lg:self-center'>
+                        <div className='lg:fixed lg:z-40'>
+                            <ImgLoader
+                                src={image}
+                                alt={'Cover of ' + title}
+                                dimensions='
                             h-[96vw]
                             w-[60vw]
          
@@ -56,84 +59,85 @@ function BookDetails({
                             lg:h-[500px]
                             lg:w-[312.5px]
                             '
-                            rounded='rounded-md'
-                        />
-                        <div className='w-full mx-auto mt-3 text-sm'>
-                            {claimed && (
-                                <p className='text-xs text-zinc-600'>
-                                    Claimed by: <span className='italic'>{claimed}</span>
-                                </p>
-                            )}
-                            {claimed ? (
-                                <button
-                                    data-element='return'
-                                    onClick={toggleReturn}
-                                    className='underline text-center text-md text-rose-400 font-bold'
-                                >
-                                    Return Book
-                                </button>
-                            ) : (
-                                <button
-                                    data-element='claim'
-                                    onClick={toggleClaim}
-                                    className='button py-2 w-full'
-                                >
-                                    Claim Book
-                                </button>
-                            )}
-                            {openReturn && claimed && (
-                                <ReturnForm
-                                    getBookData={getBookData}
-                                    claimed={claimed}
-                                    open={openReturn}
-                                    visibilityToggle={toggleReturn}
-                                    bookTitle={title}
-                                />
-                            )}
-                            {openClaim && !claimed && (
-                                <ClaimForm
-                                    getBookData={getBookData}
-                                    open={openClaim}
-                                    visibilityToggle={toggleClaim}
-                                    bookTitle={title}
-                                />
-                            )}
+                                rounded='rounded-md'
+                            />
+                            <div className='w-full mx-auto mt-3 text-sm'>
+                                {claimed && (
+                                    <p className='text-xs text-zinc-600'>
+                                        Claimed by: <span className='italic'>{claimed}</span>
+                                    </p>
+                                )}
+                                {claimed ? (
+                                    <button
+                                        data-element='return'
+                                        onClick={toggleReturn}
+                                        className='underline text-center text-md text-rose-400 font-bold'
+                                    >
+                                        Return Book
+                                    </button>
+                                ) : (
+                                    <button
+                                        data-element='claim'
+                                        onClick={toggleClaim}
+                                        className='button py-2 w-full'
+                                    >
+                                        Claim Book
+                                    </button>
+                                )}
+                                {openReturn && claimed && (
+                                    <ReturnForm
+                                        getBookData={getBookData}
+                                        claimed={claimed}
+                                        open={openReturn}
+                                        visibilityToggle={toggleReturn}
+                                        bookTitle={title}
+                                    />
+                                )}
+                                {openClaim && !claimed && (
+                                    <ClaimForm
+                                        getBookData={getBookData}
+                                        open={openClaim}
+                                        visibilityToggle={toggleClaim}
+                                        bookTitle={title}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col w-full gap-2 lg:max-w-2xl'>
+                        <h1 className='text-center lg:text-left p-0 sm:text-5xl text-[7vw] leading-tight'>
+                            {title}
+                        </h1>
+                        <p className='text-center lg:text-left'>{author}</p>
+                        <div className='flex gap-2 items-baseline m-2 self-center lg:self-start pb-4 lg:pb-0'>
+                            <p className='text-2xl'>{avgScore.toFixed(1)}</p>
+                            <StarRating rating={avgScore} />
+                            <a href='#reviews' className='underline text-zinc-500'>
+                                {reviews?.length} reviews
+                            </a>
+                        </div>
+                        <p>{blurb}</p>
+                        <br />
+                        <p>Genre: {genre.name}</p>
+                        <p>{year}</p>
+
+                        {pageCount && <p>{pageCount} pages</p>}
+                        <div className='mt-6 border-zinc-300'>
+                            <h2
+                                className='border-t border-zinc-200 text-center lg:text-left'
+                                id='reviews'
+                            >
+                                Reviews
+                            </h2>
+                            {reviews?.map((review) => (
+                                <Review review={review} key={review.id} />
+                            ))}
+                            <ReviewForm refreshReviewsList={refreshReviewsList} />
                         </div>
                     </div>
                 </div>
-                <div className='flex flex-col w-full gap-2 lg:max-w-2xl'>
-                    <h1 className='text-center lg:text-left p-0 sm:text-5xl text-[7vw] leading-tight'>
-                        {title}
-                    </h1>
-                    <p className='text-center lg:text-left'>{author}</p>
-                    <div className='flex gap-2 items-baseline m-2 self-center lg:self-start pb-4 lg:pb-0'>
-                        <p className='text-2xl'>{avgScore.toFixed(1)}</p>
-                        <StarRating rating={avgScore} />
-                        <a href='#reviews' className='underline text-zinc-500'>
-                            {reviews?.length} reviews
-                        </a>
-                    </div>
-                    <p>{blurb}</p>
-                    <br />
-                    <p>Genre: {genre.name}</p>
-                    <p>{year}</p>
-
-                    {pageCount && <p>{pageCount} pages</p>}
-                    <div className='mt-6 border-zinc-300'>
-                        <h2
-                            className='border-t border-zinc-200 text-center lg:text-left'
-                            id='reviews'
-                        >
-                            Reviews
-                        </h2>
-                        {reviews?.map((review) => (
-                            <Review review={review} key={review.id} />
-                        ))}
-                        <ReviewForm refreshReviewsList={refreshReviewsList} />
-                    </div>
-                </div>
             </div>
-        </div>
+        </>
     );
 }
 
