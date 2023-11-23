753a77c845f9bd05c46cc5eef746fedf7f335775
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-11-23 12:07:09 +0000

    Create Spinner component

--- src/Components/Bookpage/BookDetails/index.js
+++ src/Components/Bookpage/BookDetails/index.js
@@ -7,6 +7,7 @@ import { useState } from 'react';
 import StarRating from './StarRating';
 import ScrollToTop from '../../ScrollToTop';
 import LazyImgLoader from '../../LazyImgLoader';
+import Spinner from '../../Spinner';
 
 function BookDetails({
     image,
@@ -63,29 +64,7 @@ function BookDetails({
                             />
                             <div className='w-full mx-auto mt-3 text-sm'>
                                 {claimed === null ? (
-                                    <div className='flex justify-center items-center text-zinc-600 h-12'>
-                                        <svg
-                                            className='animate-spin h-5 w-5 text-slate-400 align-middle'
-                                            xmlns='http://www.w3.org/2000/svg'
-                                            fill='none'
-                                            viewBox='0 0 24 24'
-                                        >
-                                            <circle
-                                                className='opacity-25'
-                                                cx='12'
-                                                cy='12'
-                                                r='10'
-                                                stroke='currentColor'
-                                                strokeWidth='4'
-                                            ></circle>
-                                            <path
-                                                className='opacity-75'
-                                                fill='currentColor'
-                                                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                                            ></path>
-                                        </svg>
-                                        {/* &nbsp;Checking status... */}
-                                    </div>
+                                    <Spinner />
                                 ) : claimed ? (
                                     <>
                                         <p className='text-xs text-zinc-600'>
