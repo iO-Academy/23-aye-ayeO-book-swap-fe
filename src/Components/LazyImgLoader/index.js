5d0a58923359d649e4fe7b06560da14911178175
Vladi <vsstanchev@gmail.com>
2023-11-23 02:47:53 +0000

    Fix broken image flash by hiding img until loaded

--- src/Components/LazyImgLoader/index.js
+++ src/Components/LazyImgLoader/index.js
@@ -12,16 +12,14 @@ const LazyImgLoader = ({ src, alt, dimensions, rounded }) => {
     };
 
     return (
-        <div className={`flex justify-center items-center ${dimensions} ${rounded}`}>
+        <div className={`relative ${dimensions} ${rounded}`}>
             <div
                 role='status'
-                className={`flex items-center justify-center bg-gray-300 dark:bg-gray-200 ${dimensions} ${rounded} ${
+                className={`absolute inset-0 flex items-center justify-center bg-zinc-300 dark:bg-zinc-200 ${dimensions} ${rounded} ${
                     isImageLoaded ? 'animate-none' : 'animate-pulse'
                 }`}
                 style={{
-                    display: isImageLoaded || !inView ? 'none' : 'flex',
-                    width: dimensions,
-                    height: dimensions,
+                    display: isImageLoaded ? 'none' : 'flex',
                 }}
             >
                 <svg
@@ -30,7 +28,6 @@ const LazyImgLoader = ({ src, alt, dimensions, rounded }) => {
                     xmlns='http://www.w3.org/2000/svg'
                     fill='currentColor'
                     viewBox='0 0 20 18'
-                    style={{ display: isImageLoaded || !inView ? 'none' : 'block' }}
                 >
                     <path d='M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z' />
                 </svg>
@@ -42,7 +39,7 @@ const LazyImgLoader = ({ src, alt, dimensions, rounded }) => {
                 alt={alt}
                 onLoad={handleImageLoad}
                 className={`object-cover object-top ${dimensions} ${rounded}`}
-                style={{ display: isImageLoaded || !inView ? 'block' : 'none' }}
+                style={{ visibility: isImageLoaded || !inView ? 'visible' : 'hidden' }}
                 loading={inView ? 'auto' : 'lazy'}
             />
         </div>
