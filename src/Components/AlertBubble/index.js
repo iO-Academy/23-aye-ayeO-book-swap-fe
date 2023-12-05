c53cd7ead8e504499ee6dc126e95b0edd04ff9b8
Vladi <vsstanchev@gmail.com>
2023-12-05 15:32:28 +0000

    Redesign alert bubble

--- src/Components/AlertBubble/index.js
+++ src/Components/AlertBubble/index.js
@@ -5,11 +5,11 @@ function AlertBubble({ message }) {
 
     useEffect(() => {
         if (message) {
-            setPosition('-translate-y-5');
+            setPosition('translate-y-5');
 
             const timer = setTimeout(() => {
-                setPosition('translate-y-full -z-50');
-            }, 3000);
+                setPosition('-translate-y-full -z-50');
+            }, 4000);
 
             return () => {
                 clearTimeout(timer);
@@ -20,8 +20,17 @@ function AlertBubble({ message }) {
     return (
         <div className='flex items-center justify-center'>
             <div
-                className={`rounded-[30px] backdrop-blur-sm bg-slate-800/80 transition-all ease-in-out duration-1600 fixed bottom-0 z-50 sm:w-full w-[95vw] max-w-[800px] text-center text-zinc-100 text-2xl py-8 ${position}`}
+                className={`rounded-[30px] backdrop-blur-sm bg-lime-600/100 transition-all ease-in-out duration-600 fixed top-0 z-50 sm:w-max w-[95vw] max-w-[800px] text-center text-zinc-100 text-xl px-10 p-4 ${position} flex flex-row items-center justify-center gap-2`}
             >
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    height='24'
+                    viewBox='0 -960 960 960'
+                    width='24'
+                    fill='currentColor'
+                >
+                    <path d='m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z' />
+                </svg>
                 {message}
             </div>{' '}
         </div>
