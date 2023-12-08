3be8676099afe04f6f76a61e625ac99bb5847ba2
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-12-08 22:26:21 +0000

    Add pagination to bookshelf

--- src/Components/SplashScreen/index.js
+++ src/Components/SplashScreen/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import logo from '../../logo.png';
+import React from "react";
+import logo from "../../logo.png";
 
 function SplashScreen() {
-    return (
-        <div className='h-screen flex items-center justify-center bg-rose-100'>
-            <img src={logo} alt='Swapp logo' width='100px' className='animate-pulse' />
-        </div>
-    );
+  return (
+    <div className="flex h-screen items-center justify-center bg-rose-100">
+      <img
+        src={logo}
+        alt="Swapp logo"
+        width="100px"
+        className="animate-pulse"
+      />
+    </div>
+  );
 }
 
 export default SplashScreen;
