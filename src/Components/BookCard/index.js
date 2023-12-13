e916e899e638c43ca4137cbcbc00862123c6a386
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-12-13 16:27:20 +0000

    Implement dynamic title truncation

--- src/Components/BookCard/index.js
+++ src/Components/BookCard/index.js
@@ -2,8 +2,22 @@ import './bookcard.css';
 import { Link } from 'react-router-dom';
 import LazyImgLoader from '../LazyImgLoader';
 import { truncateWithEllipsis } from '../../utilities.js';
+import { useEffect, useState } from 'react';
 
 function BookCard({ id, bookCover, title, author, genre, onClick }) {
+    const [maxTitleLength, setMaxTitleLength] = useState(55);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setMaxTitleLength(window.innerWidth <= 768 ? 53 : 55);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <Link
             to={'/books/' + id}
@@ -40,7 +54,7 @@ function BookCard({ id, bookCover, title, author, genre, onClick }) {
                             className='pb-3 pt-0 text-base font-black text-slate-600'
                             title={title.length > 55 ? title : ''}
                         >
-                            {truncateWithEllipsis(title, 55)}
+                            {truncateWithEllipsis(title, maxTitleLength)}
                         </h2>
                     </div>
                     <p className='text-sm text-zinc-600'>by {author}</p>
