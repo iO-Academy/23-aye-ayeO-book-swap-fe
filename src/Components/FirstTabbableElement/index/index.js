881bb580c5a6146d174020f7b2a72d4c5d6fc3ff
Vladi <vsstanchev@gmail.com>
2023-12-22 19:08:40 +0000

    Create FirstTabbableElement

---
+++ src/Components/FirstTabbableElement/index/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FirstTabbableElement() {
+    return (
+        <div
+            id='first-tabbable-element'
+            className='-y-translate-10'
+            tabIndex={0}
+        ></div>
+    );
+}
+
+export default FirstTabbableElement;
